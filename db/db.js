735dacf7b55e5586872569363264687e412320ed
StephenVNelson <StephenVNelson@gmail.com>
2019-12-28 22:12:08 -0800

    added generateID method for clearer code

--- db/db.js
+++ db/db.js
@@ -17,16 +17,21 @@ function reSort(data) {
   return data
 }
 
+function generateID() {
+  return (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase()
+}
+
 // Create
 function createTask(task, callback) {
   loadData()
-  task.id = (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase()
+  task.id = generateID()
   task.sort = data.tasks.length + 1
   data.tasks.push(task)
   let newData = JSON.stringify(data, null, 2);
   fs.writeFile('db/db.json', newData, callback);
 }
 
+// Delete
 function deleteTask(taskID, taskSortNumber, callback) {
   let data = loadData()
   if (data.tasks[taskSortNumber - 1].id == taskID) {
